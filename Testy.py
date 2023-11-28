5730435dd4533ee9c0b57404783ce88e632ae111
Soch31 <76214440+Soch31@users.noreply.github.com>
2023-11-28 15:37:32 +0100

    Update Testy.py

--- Testy.py
+++ Testy.py
@@ -84,7 +84,7 @@ if prompt := st.chat_input("Comment puis-je vous aider ?"):
 
 # Handle run status
 if hasattr(st.session_state.run, 'status'):
-    if st.session_state.run.status == "running":
+    if st.session_state.run.status == "in_progress":
         with st.chat_message('assistant'):
             st.write("Thinking ......")
         if st.session_state.retry_error < 3:
