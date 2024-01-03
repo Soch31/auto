bdabc8d08a2d7044a0ff27f9d696be41a2046d35
Soch31 <76214440+Soch31@users.noreply.github.com>
2024-01-03 17:12:07 +0100

    Update Testy.py

--- Testy.py
+++ Testy.py
@@ -115,110 +115,110 @@ elif authentication_status:
     if authenticator.logout('Logout', 'main', key='unique_key'):
         st.write("You have been logged out!")
 
-# Initialize OpenAI client
-client = OpenAI()
-
-# Your chosen model
-MODEL = "gpt-3.5-turbo-1106"
-
-# Initialize session state variables
-if "session_id" not in st.session_state:
-    st.session_state.session_id = str(uuid.uuid4())
-
-if "run" not in st.session_state:
-    st.session_state.run = {"status": None}
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-if "retry_error" not in st.session_state:
-    st.session_state.retry_error = 0
-
-if "selected_assistant" not in st.session_state:
-    st.session_state.selected_assistant = "name"
-
-# Set up the page
-st.sidebar.image('TS.png',use_column_width=True)
-st.sidebar.title("SAM l'expert emploi")
-st.sidebar.divider()
-
-# Select Assistant
-st.session_state.selected_assistant = "asst_OmqTkgUMqoPEOX4boQ7EGxbz"
-st.sidebar.markdown("Je m’appelle Sam, je suis expert.e emploi en France. Je suis une intelligence artificielle au service de l’accompagnement Activ’Projet. Je suis là pour vous donner des idées et ouvrir des pistes de réflexion")
-st.sidebar.divider()
-
-# Initialize OpenAI assistant
-if "assistant" not in st.session_state:
-    openai.api_key = st.secrets["OPENAI_API_KEY"]
-    st.session_state.assistant = openai.beta.assistants.retrieve(st.session_state.selected_assistant)
-    st.session_state.thread = client.beta.threads.create(
-        metadata={'session_id': st.session_state.session_id}
-    )
-
-# Display chat messages
-elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == "completed":
-    st.session_state.messages = client.beta.threads.messages.list(
-        thread_id=st.session_state.thread.id
-    )
-    for message in reversed(st.session_state.messages.data):
-        if message.role in ["user", "assistant"]:
-            with st.chat_message(message.role):
-                for content_part in message.content:
-                    message_text = content_part.text.value
-                    st.markdown(message_text)
-
-# Chat input and message creation with file ID
-if prompt := st.chat_input(""):
-    with st.chat_message('user'):
-        st.write(prompt)
-
-    message_data = {
-        "thread_id": st.session_state.thread.id,
-        "role": "user",
-        "content": prompt
-    }
-
-    # Include file ID in the request if available
-    if "file_id" in st.session_state:
-        message_data["file_ids"] = [st.session_state.file_id]
-
-    st.session_state.messages = client.beta.threads.messages.create(**message_data)
-
-    st.session_state.run = client.beta.threads.runs.create(
-        thread_id=st.session_state.thread.id,
-        assistant_id=st.session_state.selected_assistant,
-    )
-    if st.session_state.retry_error < 3:
-        time.sleep(1)
-        st.rerun()
-
-# Handle run status
-if hasattr(st.session_state.run, 'status'):
-    if st.session_state.run.status == "running":
-        with st.chat_message('assistant'):
-            st.write("Thinking ......")
+    # Initialize OpenAI client
+    client = OpenAI()
+    
+    # Your chosen model
+    MODEL = "gpt-3.5-turbo-1106"
+    
+    # Initialize session state variables
+    if "session_id" not in st.session_state:
+        st.session_state.session_id = str(uuid.uuid4())
+    
+    if "run" not in st.session_state:
+        st.session_state.run = {"status": None}
+    
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    if "retry_error" not in st.session_state:
+        st.session_state.retry_error = 0
+    
+    if "selected_assistant" not in st.session_state:
+        st.session_state.selected_assistant = "name"
+    
+    # Set up the page
+    st.sidebar.image('TS.png',use_column_width=True)
+    st.sidebar.title("SAM l'expert emploi")
+    st.sidebar.divider()
+    
+    # Select Assistant
+    st.session_state.selected_assistant = "asst_OmqTkgUMqoPEOX4boQ7EGxbz"
+    st.sidebar.markdown("Je m’appelle Sam, je suis expert.e emploi en France. Je suis une intelligence artificielle au service de l’accompagnement Activ’Projet. Je suis là pour vous donner des idées et ouvrir des pistes de réflexion")
+    st.sidebar.divider()
+    
+    # Initialize OpenAI assistant
+    if "assistant" not in st.session_state:
+        openai.api_key = st.secrets["OPENAI_API_KEY"]
+        st.session_state.assistant = openai.beta.assistants.retrieve(st.session_state.selected_assistant)
+        st.session_state.thread = client.beta.threads.create(
+            metadata={'session_id': st.session_state.session_id}
+        )
+    
+    # Display chat messages
+    elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == "completed":
+        st.session_state.messages = client.beta.threads.messages.list(
+            thread_id=st.session_state.thread.id
+        )
+        for message in reversed(st.session_state.messages.data):
+            if message.role in ["user", "assistant"]:
+                with st.chat_message(message.role):
+                    for content_part in message.content:
+                        message_text = content_part.text.value
+                        st.markdown(message_text)
+    
+    # Chat input and message creation with file ID
+    if prompt := st.chat_input(""):
+        with st.chat_message('user'):
+            st.write(prompt)
+    
+        message_data = {
+            "thread_id": st.session_state.thread.id,
+            "role": "user",
+            "content": prompt
+        }
+    
+        # Include file ID in the request if available
+        if "file_id" in st.session_state:
+            message_data["file_ids"] = [st.session_state.file_id]
+    
+        st.session_state.messages = client.beta.threads.messages.create(**message_data)
+    
+        st.session_state.run = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread.id,
+            assistant_id=st.session_state.selected_assistant,
+        )
         if st.session_state.retry_error < 3:
             time.sleep(1)
             st.rerun()
-
-    elif st.session_state.run.status == "failed":
-        st.session_state.retry_error += 1
-        with st.chat_message('assistant'):
+    
+    # Handle run status
+    if hasattr(st.session_state.run, 'status'):
+        if st.session_state.run.status == "running":
+            with st.chat_message('assistant'):
+                st.write("Thinking ......")
             if st.session_state.retry_error < 3:
-                st.write("Run failed, retrying ......")
+                time.sleep(1)
+                st.rerun()
+    
+        elif st.session_state.run.status == "failed":
+            st.session_state.retry_error += 1
+            with st.chat_message('assistant'):
+                if st.session_state.retry_error < 3:
+                    st.write("Run failed, retrying ......")
+                    time.sleep(3)
+                    st.rerun()
+                else:
+                    st.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
+    
+        elif st.session_state.run.status != "completed":
+            st.session_state.run = client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread.id,
+                run_id=st.session_state.run.id,
+            )
+            if st.session_state.retry_error < 3:
                 time.sleep(3)
                 st.rerun()
-            else:
-                st.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
-
-    elif st.session_state.run.status != "completed":
-        st.session_state.run = client.beta.threads.runs.retrieve(
-            thread_id=st.session_state.thread.id,
-            run_id=st.session_state.run.id,
-        )
-        if st.session_state.retry_error < 3:
-            time.sleep(3)
-            st.rerun()
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 
