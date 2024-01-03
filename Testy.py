9a2faf5a3cdffdfa941de63941e95793c4a6efbb
Soch31 <76214440+Soch31@users.noreply.github.com>
2024-01-03 09:25:09 +0100

    Update Testy.py
    Description SAM

--- Testy.py
+++ Testy.py
@@ -39,7 +39,7 @@ st.sidebar.divider()
 
 # Select Assistant
 st.session_state.selected_assistant = "asst_OmqTkgUMqoPEOX4boQ7EGxbz"
-st.sidebar.markdown("Description SAM")
+st.sidebar.markdown("Je m’appelle Sam, je suis expert.e emploi en France. Je suis une intelligence artificielle au service de l’accompagnement Activ’Projet. Je suis là pour vous donner des idées et ouvrir des pistes de réflexion")
 st.sidebar.divider()
 
 # Initialize OpenAI assistant
